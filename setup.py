a17a2737a60addf22f6d7d0210338b416a713155
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-07-26 17:19:33 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@ import os
 import shutil
 import PyInstaller.__main__
 
+application_name = 'Minecraft server'
+icon_path = os.path.join('assets', 'img', 'icon.ico')
+
+dist_folder_path = os.path.join('dist')
+
 PyInstaller.__main__.run([
     os.path.join('main.py'),
-    '--name=%s' % 'Minecraft server',
-    '--icon=%s' % os.path.join('assets', 'img', 'icon.ico'),
+    f'--name={application_name}',
+    f'--icon={icon_path}',
     '--onefile',
 ])
 
 shutil.rmtree('build')
+os.remove(f'{application_name}.spec')
+
+shutil.copy(icon_path, dist_folder_path)
