bb113e94e8ec67e7d796e8913f484335b76b4a29
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-07-26 17:59:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ application_name = 'Minecraft server'
 icon_path = os.path.join('assets', 'img', 'icon.ico')
 ngrok_path = os.path.join('assets', 'ngrok.exe')
 
+assets_folder_path = os.path.join('assets')
 dist_folder_path = os.path.join('dist')
+assets_dist_folder_path = os.path.join(dist_folder_path, 'assets')
 
 PyInstaller.__main__.run([
     os.path.join('main.py'),
@@ -16,9 +18,13 @@ PyInstaller.__main__.run([
     '--onefile',
 ])
 
-shutil.rmtree('build')
-os.remove(f'{application_name}.spec')
+with suppress(FileNotFoundError):
+    shutil.rmtree('build')
+with suppress(FileNotFoundError):
+    os.remove(f'{application_name}.spec')
 
-with suppress(OSError):
-    shutil.copy(icon_path, dist_folder_path)
-shutil.copy(ngrok_path, dist_folder_path)
+with suppress(FileExistsError):
+    os.mkdir(assets_dist_folder_path)
+
+shutil.copy(icon_path, dist_folder_path)
+shutil.copy(ngrok_path, assets_dist_folder_path)
