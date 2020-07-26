80c7a04d1ebdbaeeaf87f03b56d40c1546db12aa
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-07-26 20:05:16 -0300

    aceita termos automaticamente

--- utils/server.py
+++ utils/server.py
@@ -1,19 +1,29 @@
 import os
+import traceback
 import subprocess
 from config import config
+from utils import console
 
 server_path = config['path']['server']
 
 
-def accept_terms():
-    if os.path.isfile('eula.txt'):
+def accept_terms() -> bool:
+    try:
         with open('eula.txt') as f:
             eula = f.read()
         with open('eula.txt', 'w') as f:
             accept_eula = eula.replace('eula=false', 'eula=true')
             f.write(accept_eula)
+        return True
+    except FileNotFoundError:
+        return False
+    except Exception:
+        traceback.print_exc()
+        return False
 
 
 def start():
-    accept_terms()
+    while not accept_terms():
+        os.system(f"java -Xmx1024M -Xms1024M -jar {server_path} nogui")
+        console.clear()
     os.system(f"java -Xmx1024M -Xms1024M -jar {server_path} nogui")
