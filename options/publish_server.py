b2d6da2e2379f9267a5e43b08106eb096885bf31
Luan Silveira <46425069+SilveiraL@users.noreply.github.com>
2020-07-26 23:51:14 -0300

    inicia ngrok na porta do servidor minecraft

--- options/publish_server.py
+++ options/publish_server.py
@@ -1,9 +1,22 @@
+import re
 from models.option import Option
 from utils import ngrok
+from utils import styledprint
+
+
+def get_port() -> int:
+    with open('server.properties') as f:
+        properties = f.read()
+        port = re.findall(r'server-port=(\d+)', properties)[0]
+        return int(port)
 
 
 class PublishServer(Option):
     name = 'Tornar o servidor público'
 
     async def run(self):
-        ngrok.tcp(25565)
+        try:
+            port = get_port()
+            ngrok.tcp(port)
+        except Exception as e:
+            styledprint.styled_print(e, color=styledprint.Color.RED)
